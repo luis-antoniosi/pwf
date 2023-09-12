bab5c5b73ec197a51a184dd8b9d07f956e8f5b35
Aluno <aluno@etec.sp.gov.br>
2023-09-12 20:51:42 -0300

    Atividade Aula 12/09/2023

---
+++ PWFilmes.API/Controllers/CategoriaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PWFilmes.Domain;
+
+namespace PWFilmes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        [HttpGet("listar")]
+        public IActionResult Listar()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria
+            {
+                Codigo = 1,
+                Descricao = "Terror",
+                Cor = "Vermelho"
+            });
+
+            categorias.Add(new Categoria
+            {
+                Codigo = 2,
+                Descricao = "Comédia",
+                Cor = "Azul"
+            });
+
+            return Ok(categorias);
+        }
+    }
+}
