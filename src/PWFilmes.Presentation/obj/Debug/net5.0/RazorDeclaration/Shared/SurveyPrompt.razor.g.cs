b14b50f1245d36c68596a2444508f3f5225ffc0d
luis-antoniosi <luis.antoniosi@etec.sp.gov.br>
2023-11-21 20:44:33 -0300

    Aula 21/11/2023 - Criação da página para adicionar categorias

--- src/PWFilmes.Presentation/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ src/PWFilmes.Presentation/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,84 +13,84 @@ namespace PWFilmes.Presentation.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 1 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 2 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 3 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 4 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 5 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 6 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 7 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 8 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 9 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using PWFilmes.Presentation;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 10 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using PWFilmes.Presentation.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 11 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\PW\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 12 "C:\PWF\pwf\src\PWFilmes.Presentation\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
@@ -104,7 +104,7 @@ using Radzen.Blazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\PW\PWFilmes\src\PWFilmes.Presentation\Shared\SurveyPrompt.razor"
+#line 12 "C:\PWF\pwf\src\PWFilmes.Presentation\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
